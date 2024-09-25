553b36cd773b94d0a05aca95d8eae080efbddaac
CJU SeokJinChoi <qweop1218@gmail.com>
2024-09-25 19:17:44 +0900

    load

--- 2nd week/01_Practice01/01_Practice01_triangle/2nd_week_practice01_triangle/2nd_week_practice01_triangle/main.cpp
+++ 2nd week/01_Practice01/01_Practice01_triangle/2nd_week_practice01_triangle/2nd_week_practice01_triangle/main.cpp
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-	double tri_base_length;
-	double tri_height;
+	double tri_base_length;                     //삼각형 밑변 변수
+	double tri_height;                          //삼각형 높이 변수
 	
 
-	printf("Input tri_base_length, tri_height:");
-	scanf_s("%lf%lf", &tri_base_length, &tri_height);
+	printf("Input tri_base_length, tri_height:");               //입력 안내 메세지 출력
+	scanf_s("%lf%lf", &tri_base_length, &tri_height);           //밑변, 높이 입력
 
-	double tri_extent = tri_base_length * tri_height/2 ;
+	double tri_extent = tri_base_length * tri_height/2 ;        //삼각형 넓이 변수
 
-	printf("tri_extent: %.1lfm^2 ",tri_extent);
+	printf("tri_extent: %.1lfm^2 ",tri_extent);                 //입력값 출력
 	return 0;
 }
