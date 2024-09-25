e6cc403805019a357b555edcbe10fc9058d20a9c
CJU SeokJinChoi <qweop1218@gmail.com>
2024-09-25 17:49:13 +0900

    initial load

---
+++ 2nd week/01_Practice01/01_Practice01_triangle/2nd_week_practice01_triangle/2nd_week_practice01_triangle/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double tri_base_length;
+	double tri_height;
+	
+
+	printf("Input tri_base_length, tri_height:");
+	scanf_s("%lf%lf", &tri_base_length, &tri_height);
+
+	double tri_extent = tri_base_length * tri_height/2 ;
+
+	printf("tri_extent: %.1lf ",tri_extent);
+	return 0;
+}
