21dd181ad401b0f312b30568bfe3182883c0eb06
CJU SeokJinChoi <qweop1218@gmail.com>
2024-09-25 18:20:08 +0900

    load

---
+++ 2nd week/01_Practice01/01_Practice01_circle/circle/circle/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	double cir_radius; 
+	double cir_pi = 3.14;                //원 반지름,파이 변수 선언
+
+	printf("Input cir_radius: ");     //입력 안내 메세지 출력
+	scanf_s("%lf", &cir_radius);      //원 반지름 입력
+
+	double cir_extent = cir_pi * cir_radius*cir_radius;     //원 넓이 공식 선언
+	printf("sq_extent:%.1lf", cir_extent);
+	
+	
+	return 0;
+}
