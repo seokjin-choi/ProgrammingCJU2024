3899bdb3f8710bbbba19acd6b0f42542cbd29699
CJU SeokJinChoi <qweop1218@gmail.com>
2024-10-15 16:49:55 +0900

    Problem2

--- 3rd week/Problem2/Problem2/main.cpp
+++ 3rd week/Problem2/Problem2/main.cpp
@@ -2,42 +2,42 @@
 
 int main(void)
 {
-	int x, y, z;
-	char operation;
+	int x, y, z;     //변수 선언
+	char operation;  //기호 선언
 
-	printf("사칙연산 입력(정수) :");
-	scanf_s("%d%c%d", &x, &operation, sizeof(operation), &y);
+	printf("사칙연산 입력(정수) :");   //식 입력 안내 문자 
+	scanf_s("%d%c%d", &x, &operation, sizeof(operation), &y); //식 입력
 
 	switch (operation)
 	{
-	case '+':
+	case '+':          //기호가 +일 때 더하고 출력
 	{
 		z = x + y;
 		printf("%d%c%d=%d", x, operation, y, z);
 		break;
 	}
 
-	case '-':
+	case '-':     //기호가 -일 때 빼고 출력
 	{
 		z = x - y;
 		printf("%d%c%d=%d", x, operation, y, z);
 		break;
 	}
 
-	case '*':
+	case '*':       //기호가 *일 때 곱하고 출력
 	{
 		z = x * y;
 		printf("%d%c%d=%d", x, operation, y, z);
 		break;
 	}
 
-	case '/':
+	case '/':     //기호가 /일 때 나누고 출력
 	{
 		z = x / y;
 		printf("%d%c%d=%d", x, operation, y, z);
 		break;
 	}
-	default:
+	default:      //전부 해당 안될 때 계산불가 출력
 	{
 		printf("계산할 수 없습니다.");
 	}
